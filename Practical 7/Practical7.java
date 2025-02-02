c02694d0293ed8af62fb55b55bd9e341c1076c8d
pratikshinde501 <2024bit501@sggs.ac.in>
2025-02-02 17:34:16 +0530

    Update Practical7.java

--- Practical 7/Practical7.java
+++ Practical 7/Practical7.java
@@ -1,106 +1,107 @@
-public class Practical7 {
-    public String printName(int size){
-        String ans="";
-        int inc=size;
-        int inc1 = size;
-        int inc2 = 5*size;
-        if(size!=1){
-            inc1=size-2;
-            inc2=(5*size)-size;
-        }
-        int prash=size%2==0?0:1;
-        for(int i=1;i<=7*size;i++){
+public class PrintNm {     
+    public String printName(int size) throws InterruptedException {         
+        int inc = size;         
+        int inc1 = 2;         
+        int inc2 = 5 + size-2;      
+        String prash="";       
 
-            //Code for P
-            for(int j=1;j<=5*size;j++){
-                if(j==1||i==1||i==7*size/2||(j==5*size&&i<=7*size/2)){
-                    ans+="*";
-                }
-                else{
-                    ans+=" ";
-                }
+        // p
+        for (int i = 1; i <= (7 + size); i++) {             
+            for (int j = 1; j <= (5 + size); j++) {                 
+                if (j == 1 || i == 1 || i == ((7 + size) / 2) || (j == (5 + size) && i <= ((7 + size) / 2))) {                     
+                    prash+="*";            
+                } else {                     
+                    prash+=" ";                  
+                }             
+            }             
+            prash+=" ";           
+
+            //r
+            for (int j = 1; j <= (5 + size); j++) {                 
+                if (i == 1 || j == 1 || i == ((7 + size) / 2) || (j == (5 + size) && i <= ((7 + size) / 2))) {                     
+                    prash+="*";                  
+                } else if (i > ((7 + size) / 2) && j == inc) {                     
+                    prash+="*";                 
+                } else {                     
+                    prash+=" ";                  
+                }             
+            }             
+            if (i >= ((7 + size) / 2)) {                 
+                inc++;             
+            }             
+            prash+=" ";             
+
+
+        //     //a
+            for (int j = 1; j <= (5 + size); j++) {                 
+                if (j == 1 || i == 1 || i == ((7 + size) / 2) || j == (5 + size)) {                     
+                    prash+="*";                  
+                } else {                     
+                    prash+=" ";                 
+                }             
+            }             
+            prash+=" ";              
+
+
+        //     //t
+            for (int j = 1; j <= (5 + size); j++) {                 
+                if (i == 1) {                     
+                    prash+="*";                  
+                } else if (j == ((size + 5) / 2+((size+5)%2))) {                     
+                    prash+="*";                  
+                } else {                     
+                    prash+=" ";                  
+                }             
+            }             
+            prash+=" ";               
+
+
+        //     //i
+            for (int j = 1; j <= (5 + size); j++) {                 
+                if (i == 1 || i == (size + 7)) {                     
+                    prash+="*";                  
+                } else if (j == ((size + 5) / 2 +((size+5)%2))) {                     
+                    prash+="*";                  
+                } else {                     
+                    prash+=" ";                  
+                }             
+            }             
+            prash+=" ";               
+
+
+            //k
+            for (int j = 1; j <= (size + 5); j++) {                 
+                if (j == 1) {                     
+                    prash+="*";                 
+                } else {                     
+                    prash+=" ";                  
+                }                 
+                if (i > ((7 + size) / 2+((size+7)%2)) && j == inc1) {                     
+                    prash+="*";                  
+                }                 
+                if (i <= ((7 + size)/2 + ((size+7)%2)) && j == inc2) {                     
+                    prash+="*";                  
+                }             
+            }             
+            if (i > ((7 + size) / 2)+((size+7)%2)) {                 
+                inc1++; 
             }
-            ans+=" ";
+            if (i <= ((7 + size) / 2)) {                 
+                inc2--;             
+            }              
 
-            //code for R
-            for (int j = 1; j <= 5 * size; j++) {
-                if (i == 1 || j == 1 || i == 7 * size / 2 || (j == 5 * size && i <= 7 * size / 2)) {
-                    ans += "*";
-                } else if (i > 7 * size / 2 && j == inc) {
-                    ans += "*"; 
-                } else {
-                    ans += " ";
-                }
-            }
-            if (i >= (7 * size) / 2) {
-                inc++;
-            }
-            ans+=" ";
+            prash+="\n";               
+            //printing each line string 
+            System.out.println(prash);
+            //to pause the execution by sec
+            Thread.sleep(1000);         
+        }     
+        return prash;
+    }      
 
-            //code for A
-                for(int j=1;j<=5*size;j++){
-                    if(j==1||i==1||i==7*size/2||j==5*size){
-                        ans+="*";
-                    }
-                    else{
-                        ans+=" ";
-                    }
-                }
-                ans+=" ";
-
-
-                //code for T
-                for(int j=1;j<=5*size;j++){
-                    if(i==1){
-                        ans+="*";
-                    }else if(j==(size*5)/2+prash){
-                        ans+="*";
-                    }else{
-                        ans+=" ";
-                    }
-                }
-                ans+=" ";
-
-
-                //code for I
-                for(int j=1;j<=5*size;j++){
-                    if(i==1||i==size*7){
-                        ans+="*";
-                    }else if(j==(size*5)/2+prash){
-                        ans+="*";
-                    }else{
-                        ans+=" ";
-                    }
-                }
-                ans+=" ";
-
-
-                //code for K
-                for (int j = 1; j <= size * 5; j++) {
-                    if (j == 1) {
-                        ans+="*";
-                    } else {
-                        ans+=" ";
-                    }
-                    if (i > 7 * size / 2 && j == inc1) {
-                        ans+="*";
-                    }
-                    if (i <= 7 * size / 2 && j == inc2) {
-                        ans+="*";
-                    }
-                }
-                if (i >= (7 * size) / 2) {
-                        inc1++;
-                }
-                if (i <= (7 * size) / 2) {
-                    inc2--;
-            }
-            ans+="\n";
-    }
-    return ans;
-}
-    public static void main(String[] args) {
-        Practical7 p=new Practical7();
-        System.out.println(p.printName(2));
-    }
+    public static void main(String[] args) throws InterruptedException {         
+        PrintNm p = new PrintNm();         
+        System.out.println();         
+        System.out.println(p.printName(1));     
+    } 
 }
