0deefb24e0e2328bf9064bc5b6c68e1ac52cbf5b
Prash <2024bit501@sggs.ac.in>
2025-02-14 10:39:08 +0000

    Practical 8 reupload

--- Practical 8/Practical8.java
+++ Practical 8/Practical8.java
@@ -1,74 +1,89 @@
-class Practical8{
-    void printPattern2(int size,int start){
-        if(start>size+1){
+class Practical_8 {
+    void printPattern2(int size, int start) {
+        if (start > size + 1) {
             return;
         }
-        for(int space=size-start;space>=0;space--){
-            System.out.print(" ");
-        }
-        for(int i=1;i<=start;i++){
-            if(start!=4){
-            System.out.print("*");
-            }else{
-                System.out.print("+");
-            }
-        }
+        printSpaces(size - start, 0);
+        printStarsOrPlus(start, 1, false);
         System.out.println();
-        printPattern2(size,start+1);
-        for(int space=size-start+1;space>=0;space--){
-            System.out.print(" ");
-        }
-        for(int i=1;i<start;i++){
-            System.out.print("/");
-        }
+        printPattern2(size, start + 1);
+        printSpaces(size - start + 1, 0);
+        printSlashes(start - 1, 1);
         System.out.println();
     }
-    void printPattern1(int size,int start){
-        if(start>size+1){
+
+    void printPattern1(int size, int start) {
+        if (start > size + 1) {
             return;
         }
-        for(int i=1;i<=start;i++){
-            System.out.print("*");
-        }
+        printStars(start, 1);
         System.out.println();
-        printPattern1(size,start+1);
-        for(int i=1;i<start;i++){
-            System.out.print("*");
-        }
+        printPattern1(size, start + 1);
+        printStars(start - 1, 1);
         System.out.println();
     }
-    void printPattern3(int size,int start){
-        if(start>=size*2){
+
+    void printPattern3(int size, int start) {
+        if (start >= size * 2) {
             return;
         }
         int res;
-        if((start>size)){
-            res=size*2-start; 
-        }else{
+        if(start>size){
+            res=size*2-start;
+        }
+        else{
             res=start;
         }
-        for(int space=1;space<=size-res;space++){
-            System.out.print(" ");
-        }
-        for(int star=1;star<res*2;star++){
-            System.out.print("*");
-        }
+        printSpaces(size - res, 1);
+        printStars(res * 2 - 1, 1);
         System.out.println();
-        printPattern3(size, start+1);
-        for(int i=1;i<=size;i++){
-            if(i==size)
-            System.out.print("+");
-            else{
-                System.out.print(" ");
-            }
-        }
+        printPattern3(size, start + 1);
+        printSpaces(size - 1, 1);
+        System.out.print("+");
         System.out.println();
     }
+
+    void printSpaces(int count, int current) {
+        if (current > count) {
+            return;
+        }
+        System.out.print(" ");
+        printSpaces(count, current + 1);
+    }
+
+    void printStars(int count, int current) {
+        if (current > count) {
+            return;
+        }
+        System.out.print("*");
+        printStars(count, current + 1);
+    }
+
+    void printStarsOrPlus(int count, int current, boolean isPlus) {
+        if (current > count) {
+            return;
+        }
+        if(isPlus){
+            System.out.print("+");
+        }
+        else{
+            System.out.print("*");
+        }
+        printStarsOrPlus(count, current + 1, isPlus);
+    }
+
+    void printSlashes(int count, int current) {
+        if (current > count) {
+            return;
+        }
+        System.out.print("/");
+        printSlashes(count, current + 1);
+    }
+
     public static void main(String[] args) {
-        Practical8 p1=new Practical8();
-        
-        p1.printPattern1(3,1);
-        p1.printPattern2(3,1);
+        Practical_8 p1 = new Practical_8();
+        p1.printPattern1(3, 1);
+        p1.printPattern2(3, 1);
         p1.printPattern3(3, 1);
     }
 }
